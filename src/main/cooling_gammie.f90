814bc5f9148487bbf9138d191f2b84f4a56fc69b
Veronesi Benedetta Maria Augusta <bverones@cralcalc.cral.ens-lyon.fr>
2023-02-22 07:25:01 +0100

    added option to use a modified gammie coolint in inner region

--- src/main/cooling_gammie.f90
+++ src/main/cooling_gammie.f90
@@ -20,7 +20,7 @@ module cooling_gammie
 ! :Dependencies: infile_utils, io
 !
  implicit none
- real, private :: beta_cool  = 3.
+ real, private :: beta_cool  = 3., beta_cool_in = 3., rcav = 5.
 
 contains
 !-----------------------------------------------------------------------
@@ -33,11 +33,19 @@ subroutine cooling_Gammie_explicit(xi,yi,zi,ui,dudti)
  real, intent(in)    :: ui,xi,yi,zi
  real, intent(inout) :: dudti
 
- real :: omegai,r2,tcool1
+ real :: omegai,r2,tcool1,beta_mod
 
  r2     = xi*xi + yi*yi + zi*zi
  Omegai = r2**(-0.75)
- tcool1 = Omegai/beta_cool
+
+ if (r2 <= rcav**2) then
+      beta_mod = max( beta_cool*(r2/rcav**2), beta_cool_in)
+      tcool1 = Omegai/beta_mod
+ else
+      tcool1 = Omegai/beta_cool
+ endif
+
+ !tcool1 = Omegai/beta_cool
  dudti  = dudti - ui*tcool1
 
 end subroutine cooling_Gammie_explicit
@@ -52,6 +60,8 @@ subroutine write_options_cooling_gammie(iunit)
  integer, intent(in) :: iunit
 
  call write_inopt(beta_cool,'beta_cool','beta factor in Gammie (2001) cooling',iunit)
+ call write_inopt(beta_cool_in,'beta_cool_in','beta factor inside the cavity',iunit)
+ call write_inopt(rcav, 'rcav', 'size of the expected cavity',iunit)
 
 end subroutine write_options_cooling_gammie
 
@@ -74,6 +84,12 @@ subroutine read_options_cooling_gammie(name,valstring,imatch,igotall,ierr)
     read(valstring,*,iostat=ierr) beta_cool
     ngot = ngot + 1
     if (beta_cool < 1.) call fatal('read_options','beta_cool must be >= 1')
+ case('beta_cool_in')
+    read(valstring,*,iostat=ierr) beta_cool_in
+    ngot = ngot + 1
+ case('rcav')
+    read(valstring,*,iostat=ierr) rcav
+    ngot = ngot + 1
  case default
     imatch = .false.
  end select
